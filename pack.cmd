336b127292d32fe40a5a36f20f7954fd90adec97
Licshee <licshee@yen.ai>
2016-03-18 16:03:05 +0800

    rename to LinqX as LinqEx is already taken

--- pack.cmd
+++ pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd Release
-powershell -ExecutionPolicy Bypass -File "..\package.ps1" "..\LinqEx.nuspec" LinqEx
+powershell -ExecutionPolicy Bypass -File "..\package.ps1" "..\LinqEx.nuspec" LinqX
 popd
 echo.
 echo Done.
