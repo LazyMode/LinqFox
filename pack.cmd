fba70c3697c4f3b4ff1658c6f07d542c3aaecb99
Licshee <licshee@yen.ai>
2016-03-30 14:08:50 +0800

    rename to LinqFox

--- pack.cmd
+++ pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd Release
-powershell -ExecutionPolicy Bypass -File "..\package.ps1" "..\LinqEx.nuspec" Portable\Linq Net20\Linq Net35\Linq Net40\Linq SL\Linq UWP\Linq
+powershell -ExecutionPolicy Bypass -File "..\package.ps1" "..\LinqFox.nuspec" Portable\LinqFox
 popd
 echo.
 echo Done.
